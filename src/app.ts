fd79c34ff35bb95e3899317e73605e4f6d323d4f
Mentos302 <igormelnyk302@gmail.com>
2021-03-23 01:12:34 +0200

    models & crud commit

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import config from './config'
-import db from './db'
 import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser'
+import { Op } from 'sequelize'
+import db from './db'
+import logging from './utils'
+import { Shop, Cashier, CashRegister } from './models'
+
 
 const app: Application = express()
 
@@ -13,6 +18,64 @@ db.authenticate()
     })  
     .catch(e => console.log(e))
 
+// CRUD communication
+// app.use('/', (req: Request, res: Response) => logging.info(`Server`, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`))
 
+app.get('/cashier', (req: Request, res: Response) =>  {
+    Cashier.findAll({
+        where: req.query,
+        raw: true
+        })  
+        .then(shops => console.log(shops)) 
+        .catch(e => res.send(e))
+    }  
+)
+app.post('/cashier', (req: Request, res: Response) =>  {
+    Cashier.create({ 
+        name: req.body.name, 
+        age: req.body.name,
+        sex: req.body.sex,
+        workInShifts: req.body.workInShifts,
+        shop: req.body.shop,
+        cashRegister: req.body.cashRegister,
+        yearsOfExperience: req.body.yearsOfExperience
+    })  
+    .then(() => console.log(`GOOD`)) 
+    .catch(e => res.send(e))
+})
+app.put('/cashier/:id', (req: Request, res: Response) =>  {
+    Cashier.update({ 
+        name: req.body.name, 
+        age: req.body.name,
+        sex: req.body.sex,
+        workInShifts: req.body.workInShifts,
+        shop: req.body.shop,
+        cashRegister: req.body.cashRegister,
+        yearsOfExperience: req.body.yearsOfExperience
+        }, {
+            where: {
+                id: req.params.id
+           }
+        })  
+    .then(() => console.log(`GOOD`)) 
+    .catch(e => res.send(e))
+})
+app.delete('/cashier/:id', (req: Request, res: Response) =>  {
+    Cashier.destroy({
+            where: {
+                id: req.params.id
+           }
+        })  
+    .then(() => console.log(`GOOD`)) 
+    .catch(e => res.send(e))
+})
 
-
+async function getTargetCashiers1(){
+    Cashier.findAll({
+        where: {
+            yearsOfExperience:{
+                [Op.gt]: 5
+            }
+        }
+    })
+}
--- src/models.ts
+++ src/models.ts
@@ -1,3 +1,89 @@
-// interface Shop{
-//     Name: string
-// }
+import db from './db'
+import { DataTypes, Model } from 'sequelize'
+
+interface IShop{
+    name: string,
+    city: string,
+    adress: string
+}
+const Shop = db.define('Shops', {
+  name: {
+    type: DataTypes.STRING
+  },
+  city: {
+    type: DataTypes.STRING
+  },
+  adress: {
+    type: DataTypes.STRING
+  }
+}, 
+  {
+    timestamps: false
+  }
+);
+
+interface ICashier{
+    name: string,
+    age: number,
+    sex: SexEnum,
+    workInShifts: ShiftsEnum,
+    shop: number,
+    cashRegister: number
+}
+const Cashier = db.define('Cashier', {
+    name: {
+      type: DataTypes.STRING,
+    },
+    age: {
+      type: DataTypes.INTEGER
+    },
+    sex: {
+      type: DataTypes.STRING
+    },
+    yearsOfExperience: {
+      type: DataTypes.INTEGER
+    },
+    workInShifts: {
+        type: DataTypes.STRING
+    },
+    shop: {
+        type: DataTypes.INTEGER
+    },
+    cashRegister: {
+        type: DataTypes.INTEGER
+    }
+}, {
+    timestamps: false
+});
+
+
+interface CashRegister{
+    shop: number,
+    regNumber: number
+}
+const CashRegister = db.define('CashRegister', {
+    shop: {
+      type: DataTypes.INTEGER
+    },
+    regNumber: {
+      type: DataTypes.INTEGER
+    }
+}, {
+    timestamps: false
+});
+
+enum SexEnum{
+    Male,
+    Female
+}
+enum ShiftsEnum{
+    Day = '8:00 - 16:00',
+    Evening = '16:00 - 00:00',
+    Night = '00:00 - 8:00'
+}
+
+export {
+  Shop,
+  Cashier,
+  CashRegister
+}
